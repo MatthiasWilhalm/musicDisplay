7529888af71cd566461bcea958d4a21b81203439
MatthiasWilhalm <matthias.wilhalm@gmail.com>
2024-09-30 23:34:43 +0200

    add use primary album cover color as bg

--- v2/main.js
+++ v2/main.js
@@ -40,7 +40,7 @@ const makeRequest = async () => {
     }
 }
 
-const updateDisplay = (track) => {
+const updateDisplay = async (track) => {
     const { artist, name, album, image } = track;
 
     titleDisplay.textContent = name;
@@ -53,14 +53,35 @@ const updateDisplay = (track) => {
     if(isImage(image)) {
         albumDisplayImg.src = image;
         albumDisplayImg.style.display = 'unset';
-    } else
+        const color = await updateColor(null, image);
+        updateTextColor(color);
+    } else {
         albumDisplayImg.style.display = 'none';
-    updateColor();
+        const color = await updateColor();
+        updateTextColor(color);
+    }
 }
 
-const updateColor = (color) => {
-    trackDisplay.style.backgroundColor = color ?? getColor();
-    imgFade.style.background = `linear-gradient(270deg, rgba(255,255,255,0) 49%, ${color ?? getColor()} 100%)`;    
+const updateColor = async (color, imgUrl) => {
+    color = await getColor(imgUrl);
+    trackDisplay.style.backgroundColor = color;
+    imgFade.style.background = `linear-gradient(270deg, rgba(255,255,255,0) 49%, ${color} 100%)`;
+    return color;
+}
+
+const updateTextColor = (color) => {
+    const brightness = getBrightness(color);
+    if(brightness < 125 || !color) {
+        titleDisplay.style.color = "white";
+        trackTitleCloneDisplay.style.color = "white";
+        artistDisplay.style.color = "white";
+        albumDisplay.style.color = "white";
+    } else {
+        titleDisplay.style.color = "black";
+        trackTitleCloneDisplay.style.color = "black";
+        artistDisplay.style.color = "black";
+        albumDisplay.style.color = "black";
+    }
 }
 
 const isImage = (url) => {
@@ -70,20 +91,22 @@ const isImage = (url) => {
     return true;
 }
 
-const toggleUpdate = e => {
+const toggleUpdate = async e => {
     if(updateIntervatFunction) {
         clearInterval(updateIntervatFunction);
         updateIntervatFunction = null;
-        updateColor('rgba(56, 56, 56, 1)');
+        await updateColor('rgba(56, 56, 56, 1)');
     } else {
         makeRequest();
         updateIntervatFunction = setInterval(makeRequest, 15000);
-        updateColor();
+        await updateColor();
     }
 }
 
-const getColor = () => {
-    return 'rgba(141, 47, 241, 1)';
+const getColor = async (imgUrl) => {
+    if(!imgUrl) return 'rgba(141, 47, 241, 1)';
+
+    return await getPrimaryColor(imgUrl);
     // return 'rgba(241, 47, 66, 1)';
 }
 
@@ -113,3 +136,57 @@ fetch('../environment.json').then(res => res.json()).then(data => {
     URL = `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=1&nowplaying="true"&api_key=${apiKey}&user=${username}&format=json`;
     init();
 }).catch(err => console.log('Error fetching environment.json'));
+
+// document.getElementById('trackAlbumImg').addEventListener('change', e => updateBgColor(e.target.src));
+
+const getPrimaryColor = async (imageUrl) => {
+    return new Promise((resolve, reject) => {
+        console.log('Image URL:', imageUrl);
+    
+        const img = new Image();
+        img.crossOrigin = 'Anonymous'; // This is important for cross-origin images
+        img.src = imageUrl;
+        img.onload = () => {
+            const canvas = document.getElementById('canvas');
+            const ctx = canvas.getContext('2d');
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img, 0, 0, img.width, img.height);
+    
+            const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+            const data = imageData.data;
+            const colorCount = {};
+            let maxCount = 0;
+            let primaryColor = '';
+    
+            for (let i = 0; i < data.length; i += 4) {
+                const r = data[i];
+                const g = data[i + 1];
+                const b = data[i + 2];
+                const color = `rgb(${r},${g},${b})`;
+    
+                if (!colorCount[color]) {
+                    colorCount[color] = 0;
+                }
+                colorCount[color]++;
+    
+                if (colorCount[color] > maxCount) {
+                    maxCount = colorCount[color];
+                    primaryColor = color;
+                }
+            }
+    
+            console.log('Primary Color:', primaryColor);
+            resolve(primaryColor);
+        };
+
+    });
+}
+
+const getBrightness = (color) => {
+    const rgb = color.match(/\d+/g);
+    const r = parseInt(rgb[0]);
+    const g = parseInt(rgb[1]);
+    const b = parseInt(rgb[2]);
+    return (r * 299 + g * 587 + b * 114) / 1000;
+}
